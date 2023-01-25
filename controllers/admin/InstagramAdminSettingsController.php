ca9906f23cf4af6cb6051c0f8c43ea92925977af
Tobiasz <shadownxr1@gmail.com>
2023-01-25 15:13:36 +0100

    HelperForm in Settings (not working)

--- controllers/admin/InstagramAdminSettingsController.php
+++ controllers/admin/InstagramAdminSettingsController.php
@@ -34,8 +34,9 @@ class InstagramAdminSettingsController extends ModuleAdminController
 
     public function initContent()
     {
+        $this->renderForm();
         parent::initContent();
-        
+        //$this->renderForm();
     }
 
     public function postProcess(){
@@ -74,6 +75,64 @@ class InstagramAdminSettingsController extends ModuleAdminController
         return parent::postProcess();
     }
 
+    public function renderForm(){
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+        $helper->override_folder = 'settings/';
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'save_settings';
+        $helper->currentIndex = $this->context->link->getAdminLink('InstagramAdminSettings', false);
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => array('instagram_app_secret' => '',
+                                    'instagram_app_id' => '',
+                                    'instagram_code' => ''
+                                ),
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    protected function getConfigForm() {
+        return array(
+            'form' => array(
+                'legend' => array(
+                    'title' => $this->l('Add Instagram Account'),
+                    'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'text',
+                        'label' => $this->l('Instagram App ID'),
+                        'name' => 'instagram_app_id',
+                    ),
+                    array(
+                        'type' => 'text',
+                        'label' => $this->l('Instagram App Secret'),
+                        'name' => 'instagram_app_secret',
+                    ),
+                    array(
+                        'type' => 'text',
+                        'label' => $this->l('Instagram Code'),
+                        'name' => 'instagram_code',
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
     public function renderList(){
         $id_settings = 1;
         $values = new instagramDisplaySettings($id_settings);
@@ -82,7 +141,10 @@ class InstagramAdminSettingsController extends ModuleAdminController
             'is_connected' => $this->db_checkIfAccessTokenExists(),
             'set_values' => $values,
         ));
-        return $this->context->smarty->fetch(_PS_MODULE_DIR_.'instagram/views/templates/admin/settings.tpl');
+        
+        $output = $this->context->smarty->fetch(_PS_MODULE_DIR_.'instagram/views/templates/admin/settings.tpl');
+
+        return $output;
     }
 
     private function db_checkIfAccessTokenExists(): bool{
