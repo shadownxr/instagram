0b55e7859713ac900375792f44aeedeba9d0f841
Tobiasz <shadownxr1@gmail.com>
2023-02-06 14:45:37 +0100

    Better json data fetch and handling

--- controllers/front/ajax.php
+++ controllers/front/ajax.php
@@ -7,19 +7,24 @@ class InstagramAjaxModuleFrontController extends ModuleFrontController {
     }
 
     public function displayAjax(){
-        if(!$this->context->isMobile()){
-            $settings = new InstagramDisplaySettings(INSTAGRAM_DESKTOP_CONFIG_ID);
-        } else {
-            $settings = new InstagramDisplaySettings(INSTAGRAM_MOBILE_CONFIG_ID);
-        }
-        $image_size = $settings->image_size;
-        $images_per_gallery = $settings->images_per_gallery;
-        $gap = $settings->gap;
-        die(Tools::jsonEncode(array(
-            'image_size' => $image_size,
-            'images_per_gallery' => $images_per_gallery,
-            'gap' => $gap
-            ))
+        $settings = new InstagramDisplaySettings(INSTAGRAM_DESKTOP_CONFIG_ID);
+
+        $array_settings = array();
+
+        $array_settings[] = array(
+            'image_size' => $settings->image_size,
+            'images_per_gallery' => $settings->images_per_gallery,
+            'gap' => $settings->gap
         );
+
+        $settings = new InstagramDisplaySettings(INSTAGRAM_MOBILE_CONFIG_ID);
+
+        $array_settings[] = array(
+            'image_size' => $settings->image_size,
+            'images_per_gallery' => $settings->images_per_gallery,
+            'gap' => $settings->gap
+        );
+
+        die(Tools::jsonEncode($array_settings));
     }
 }
--- resources/js/slider.ts
+++ resources/js/slider.ts
@@ -3,7 +3,8 @@ import '@splidejs/splide/css';
 import Splide from '@splidejs/splide';
 import UAParser from 'ua-parser-js';
 
-
+const DESKTOP = 0;
+const MOBILE = 1;
 let url = 'https://prestashop1788.local/pl/module/instagram/ajax';
 
 function fetchSettings() {
@@ -20,23 +21,31 @@ function fetchSettings() {
     });
 }
 
+function getOptions(response: any, type: number) {
+    let options = {
+        type: 'slide',
+        perPage: response[type].images_per_gallery,
+        perMove: 1,
+        width: (response[type].images_per_gallery * response[type].image_size) + Number(response[type].gap),
+        gap: Number(response[type].gap),
+    };
+
+    return options;
+}
+
 $.when(fetchSettings()).done(function(response){
     response = JSON.parse(response);
+
+    console.log(response);
 
     let parser = new UAParser();
     let device = parser.getResult()['device'];
 
-    let options = {
-        type: 'slide',
-        perPage: response.images_per_gallery,
-        perMove: 1,
-        width: (response.images_per_gallery * response.image_size) + Number(response.gap),
-        gap: Number(response.gap),
-    };
-
     if(device['type'] !== 'mobile' && device['type'] !== 'tablet'){
+        let options = getOptions(response, DESKTOP);
         new Splide( '#desktop_slider', options).mount();
     } else {
+        let options = getOptions(response, MOBILE);
         new Splide( '#mobile_slider', options).mount();
     }
 
