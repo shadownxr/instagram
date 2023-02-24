063c6e5eacf02f1f4e6bc109338f4c149a2c4b30
Tobiasz <shadownxr1@gmail.com>
2023-02-24 16:43:54 +0100

    Fix for notifications

--- resources/js/settings/notification.ts
+++ resources/js/settings/notification.ts
@@ -3,7 +3,6 @@ enum Version {
     mobile = 'm_',
 }
 
-// #todo notification hides when changing back selection of one input while both where selected
 export function notification() {
     const notification = document.querySelector('#desktop_settings #settings_notification') as HTMLDivElement;
     const m_notification = document.querySelector('#mobile_settings #settings_notification') as HTMLDivElement;
@@ -13,23 +12,42 @@ export function notification() {
 }
 
 function handleEvents(version: Version, notification: HTMLDivElement) {
-    const display_style = document.querySelectorAll('input[name=' + version + 'display_style]') as NodeListOf<HTMLInputElement>;
-    const show_title = document.querySelectorAll('input[name=' + version + 'show_title]') as NodeListOf<HTMLInputElement>;
+    const display_settings = document.querySelectorAll('input[name=' + version + 'display_style], input[name=' + version + 'show_title]') as NodeListOf<HTMLInputElement>;
 
-    addEvents(display_style, notification);
-    addEvents(show_title, notification);
+    addEvents(display_settings, notification);
 }
 
+class RadioButton {
+    radio_inputs : NodeListOf<HTMLInputElement>;
+    state: Boolean = false;
+
+    constructor(radio_inputs: NodeListOf<HTMLInputElement>) {
+        this.radio_inputs = radio_inputs;
+    }
+
+    handle(){
+        this.radio_inputs.forEach((item, index) => {
+            item.addEventListener('change', () => {
+                this.setState();
+            });
+        });
+    }
+
+    setState(){
+        this.state = !this.state;
+    }
+}
 function addEvents(elements: NodeListOf<HTMLInputElement>, notification: HTMLDivElement) {
+    let change : Boolean[] = [false, false];
     elements.forEach((item, index) => {
-        if (!item.checked) {
-            item.addEventListener('click', () => {
-                notification.innerHTML = '<p>Save to see changes<p>';
-            });
-        } else {
-            item.addEventListener('click', () => {
+        let radio_id = Math.floor(index/2);
+        item.addEventListener('change', () => {
+            change[radio_id] = !change[radio_id];
+            if(JSON.stringify(change) !== JSON.stringify([false, false])){
+                notification.innerHTML = '<p>Save to see changes</p>';
+            } else {
                 notification.innerHTML = '';
-            });
-        }
+            }
+        });
     });
 }
