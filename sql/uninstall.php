320ba721c3456baf80fd70ff906b698a51f280d4
Tobiasz <shadownxr1@gmail.com>
2023-06-23 15:09:26 +0200

    Saving images locally

--- sql/uninstall.php
+++ sql/uninstall.php
@@ -33,9 +33,10 @@ $sql = array();
 
 $sql[] = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'arkon_instagram_displaysettings`';
 $sql[] = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'arkon_instagram_images`';
+$sql[] = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'arkon_instagram_configuration`';
 
 foreach ($sql as $query) {
-    if (Db::getInstance()->execute($query) == false) {
+    if (!Db::getInstance()->execute($query)) {
         return false;
     }
 }
