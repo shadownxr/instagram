5b8559aa0ad8eb94dbea1cb7766cf1f60135b544
Tobiasz <shadownxr1@gmail.com>
2023-06-29 14:33:14 +0200

    Convert bytes to hex

--- src/Encryption/Encryption.php
+++ src/Encryption/Encryption.php
@@ -6,11 +6,12 @@ class Encryption
 {
     const CIPHER = 'AES-256-CBC';
     const IV = 's*74f65A)9#fiS5w';
+    const KEY_LENGTH = 32;
     const KEY_FILEPATH = _PS_MODULE_DIR_ . '/instagram/src/Encryption/key.txt';
 
     public static function generateKey(): bool
     {
-        $passphrase = openssl_random_pseudo_bytes(32);
+        $passphrase = bin2hex(openssl_random_pseudo_bytes(self::KEY_LENGTH));
         $fp = fopen(_PS_MODULE_DIR_ . '/instagram/src/Encryption/key.txt', 'w');
 
         if (!$fp) {
@@ -26,45 +27,6 @@ class Encryption
         }
 
         return true;
-    }
-
-    public static function encrypt(string $data, bool $random_iv = false, string &$iv = '')
-    {
-        $passphrase = (new Encryption)->getPassphrase();
-        if (empty($passphrase)) {
-            return false;
-        }
-
-        if (!$random_iv) {
-            return openssl_encrypt($data, self::CIPHER, $passphrase, 0, self::IV);
-        }
-
-        $ivlen = openssl_cipher_iv_length(self::CIPHER);
-        $isCryptoStrong = false;
-
-        do {
-            $iv = openssl_random_pseudo_bytes($ivlen, $isCryptoStrong);
-        } while(mb_strlen($iv, '8bit') !== $ivlen);
-
-        if (!$isCryptoStrong) {
-            return false;
-        }
-
-        return openssl_encrypt($data, self::CIPHER, $passphrase, 0, $iv);
-    }
-
-    public static function decrypt(string $data, string $iv = '')
-    {
-        $passphrase = (new Encryption)->getPassphrase();
-        if (empty($passphrase)) {
-            return false;
-        }
-
-        if (empty($iv)) {
-            return openssl_decrypt($data, self::CIPHER, $passphrase, 0, self::IV);
-        } else {
-            return openssl_decrypt($data, self::CIPHER, $passphrase, 0, $iv);
-        }
     }
 
     private function getPassphrase()
@@ -83,6 +45,43 @@ class Encryption
             return false;
         }
 
-        return $passphrase;
+        return hex2bin($passphrase);
+    }
+
+    public static function encrypt(string $data, bool $random_iv = false, string &$iv = '')
+    {
+        $passphrase = (new Encryption)->getPassphrase();
+        if (empty($passphrase)) {
+            return false;
+        }
+
+        if (!$random_iv) {
+            return openssl_encrypt($data, self::CIPHER, $passphrase, 0, self::IV);
+        }
+
+        $ivlen = openssl_cipher_iv_length(self::CIPHER);
+        $isCryptoStrong = false;
+
+        $iv = bin2hex(openssl_random_pseudo_bytes($ivlen, $isCryptoStrong));
+
+        if (!$isCryptoStrong) {
+            return false;
+        }
+
+        return openssl_encrypt($data, self::CIPHER, $passphrase, 0, hex2bin($iv));
+    }
+
+    public static function decrypt(string $data, string $iv = '')
+    {
+        $passphrase = (new Encryption)->getPassphrase();
+        if (empty($passphrase)) {
+            return false;
+        }
+
+        if (empty($iv)) {
+            return openssl_decrypt($data, self::CIPHER, $passphrase, 0, self::IV);
+        } else {
+            return openssl_decrypt($data, self::CIPHER, $passphrase, 0, hex2bin($iv));
+        }
     }
 }
