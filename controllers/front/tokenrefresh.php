cb91e6a2f06bdd23605c2513bf1b6a9cd033ff11
Tobiasz <shadownxr1@gmail.com>
2023-07-24 12:45:12 +0200

    Refactoring

--- controllers/front/tokenrefresh.php
+++ controllers/front/tokenrefresh.php
@@ -1,13 +1,16 @@
 <?php
 
+use ArkonInstagram\Encryption;
 use Curl\InstagramCurl;
 
-class ArkonInstagramTokenRefreshModuleFrontController extends ModuleFrontController {
+class ArkonInstagramTokenRefreshModuleFrontController extends ModuleFrontController
+{
 
-    public function initContent(){
+    public function initContent()
+    {
         $message = $this->refreshAccessToken();
 
-        switch($message) {
+        switch ($message) {
             case 'Token refreshed':
                 header('Content-Type: application/json');
                 http_response_code(200);
@@ -18,7 +21,7 @@ class ArkonInstagramTokenRefreshModuleFrontController extends ModuleFrontControl
                         'status' => 200
                     ]
                 ));
-            
+
             case 'Unable to refresh access token':
                 header('Content-Type: application/json');
                 http_response_code(500);
@@ -43,24 +46,26 @@ class ArkonInstagramTokenRefreshModuleFrontController extends ModuleFrontControl
         }
     }
 
-    private function refreshAccessToken(){
-        $response = DB::getInstance()->executeS('SELECT token_expires, creation_date FROM `' . _DB_PREFIX_ .'instagram` WHERE id_instagram='.INSTAGRAM_CONFIG_ID);
+    private function refreshAccessToken()
+    {
+        $response = DB::getInstance()->executeS('SELECT token_expires, creation_date FROM `' . _DB_PREFIX_ . 'arkon_instagram_configuration` WHERE id_instagram=' . INSTAGRAM_CONFIG_ID);
 
-        $expiration_time = (int)$response[0]['token_expires'] + idate('U',strtotime($response[0]['creation_date']));
+        $expiration_time = (int)$response[0]['token_expires'] + idate('U', strtotime($response[0]['creation_date']));
         $today_time = date("U");
 
         $access_token = $this->getAccessToken();
 
         $month_in_seconds = 2629743;
 
-        if(($expiration_time - $today_time) < $month_in_seconds){
-            $url = 'https://graph.instagram.com/refresh_access_token?access_token='.$access_token
-                .'&grant_type=ig_refresh_token';
+        if (($expiration_time - $today_time) > $month_in_seconds) {
+            $url = 'https://graph.instagram.com/refresh_access_token?access_token=' . $access_token
+                . '&grant_type=ig_refresh_token';
 
             $data = InstagramCurl::fetch($url);
-            if(!empty($data)){
-                $response = DB::getInstance()->update('instagram', array(
-                    'access_token' => $data['access_token'],
+
+            if (!empty($data)) {
+                $response = DB::getInstance()->update('arkon_instagram_configuration', array(
+                    'access_token' => Encryption::encrypt($data['access_token']),
                     'token_expires' => $data['expires_in'],
                 ));
                 return 'Token refreshed';
@@ -71,8 +76,9 @@ class ArkonInstagramTokenRefreshModuleFrontController extends ModuleFrontControl
         return 'Token not one month old';
     }
 
-    private function getAccessToken(): string{
-        $response = DB::getInstance()->executeS('SELECT access_token FROM `' . _DB_PREFIX_ .'instagram` WHERE id_instagram='.INSTAGRAM_CONFIG_ID);
-        return $response[0]['access_token'];
+    private function getAccessToken(): string
+    {
+        $response = DB::getInstance()->executeS('SELECT access_token, access_token_iv FROM `' . _DB_PREFIX_ . 'arkon_instagram_configuration` WHERE id_instagram=' . INSTAGRAM_CONFIG_ID);
+        return Encryption::decrypt($response[0]['access_token'], $response[0]['access_token_iv']);
     }
 }
